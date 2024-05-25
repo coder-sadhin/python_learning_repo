2ba9bdbf81c1297f001069a1de8948f70abf12fc
akkas_dev <codersadhin@gmail.com>
2024-05-25 17:57:06 +0800

    complete global_local vareable, os model, virtual env, incode decode game

---
+++ __main__/func.py
@@ -0,0 +1,10 @@
+def main():
+    # Code to be run when the script is run directly
+    print("Running script directly")
+if __name__ == "__main__":
+    main()
+
+# def main():
+#     print("Running script directly")
+# if __name__ == "__main__":
+#     main()
