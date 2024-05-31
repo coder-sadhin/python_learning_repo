8f11c695c4b35d1f5de56c12b17bf9e09c1e7cc6
akkas_dev <codersadhin@gmail.com>
2024-05-31 23:59:15 +0800

    super keyword in Python

---
+++ classesAndObject/helpingMethod.py
@@ -0,0 +1,12 @@
+# dir, __dict__ and help method in Python
+class Person:
+  def __init__(self, name, age):
+      self.name = name
+      self.age = age
+      self.version = 1
+
+    
+p = Person("John", 30)
+print(p.__dict__)
+
+print(help(Person))
