50ecc307e5d1739488de47ab46474b7b71b984a3
akkas_dev <codersadhin@gmail.com>
2024-06-02 23:12:44 +0800

    change some line of code

--- map_filter_reduce/filter.py
+++ map_filter_reduce/filter.py
@@ -1,8 +1,8 @@
 def fil(x):
     return x>4
 lst=[2,3,5,7,8]
-newfilf= list(filter(fil,lst))
-print(newfilf)
+newfilfee= list(filter(fil,lst))
+print(newfilfee)
 # this functon works like a water filter.
 # if you at some conditon and use filter.
 # then you gate your expacted value
