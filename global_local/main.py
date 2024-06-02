18764613c73b029ecd2f667366a05a37d290a2cd
akkas_dev <codersadhin@gmail.com>
2024-06-02 23:09:37 +0800

    change some code

--- global_local/main.py
+++ global_local/main.py
@@ -6,7 +6,7 @@ def my_function():
   x = 5  # this will change the value of the global variable x
   y = 5  # local variable
 
-
+# some change
 my_function()
 print(x)  # prints 5
 # print(y) # this will cause an error because y is a local variable and is not accessible outside of the function
