f39018dd30129551c74d83a0d77d38c72a52a4fc
akkas_dev <codersadhin@gmail.com>
2024-06-02 23:16:07 +0800

    try chach mathods

--- try_exept/try_exe.py
+++ try_exept/try_exe.py
@@ -6,11 +6,7 @@ print(f"Multiplication of {n}")
 #     print(f"{int(n)} * {i} = {int(n)*i}")
 
 # if issu some eroor
-try:
-    for i in range(1,11):
-        print(f"{int(n)} * {i} = {int(n)*i}")
-except Exception as e:
-    print("sorry, I have some error")
+
 
 
 
