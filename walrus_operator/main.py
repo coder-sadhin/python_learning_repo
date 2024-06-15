696ce35ec60a4deddef09efd4ffbf530185fd9a3
akkas_dev <codersadhin@gmail.com>
2024-06-15 23:34:07 +0800

    oparetor

--- walrus_operator/main.py
+++ walrus_operator/main.py
@@ -9,13 +9,13 @@
 
 # print(happy := True)
 
-# foods = list()
-# while True:
-#   food = input("What food do you like?: ")
-#   if food == "quit":
-#       break
-#   foods.append(food)
 foods = list()
-while (food := input("What food do you like?: ")) != "quit":
-    foods.append(food)
+while True:
+  food = input("What food do you like?: ")
+  if food == "quit":
+      break
+  foods.append(food)
+# foods = list()
+# while (food := input("What food do you like?: ")) != "quit":
+#     foods.append(food)
 # end
