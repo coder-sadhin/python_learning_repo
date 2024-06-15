43bc1f9ffe391862f66978da0a19a3a0b9704b90
akkas_dev <codersadhin@gmail.com>
2024-06-15 23:32:29 +0800

    change on time module

--- time_module/main.py
+++ time_module/main.py
@@ -19,11 +19,11 @@ import time
 # print(t1)
 
 
-# print(4)
-# time.sleep(3)
-# print("This is printed after 3 seconds")
+print(4)
+time.sleep(3)
+print("This is printed after 3 seconds")
  
-t = time.localtime()
-formatted_time = time.strftime("%Y-%m-%d %H:%M:%S", t)
+# t = time.localtime()
+# formatted_time = time.strftime("%Y-%m-%d %H:%M:%S", t)
 
-print(formatted_time) 
+# print(formatted_time) 
