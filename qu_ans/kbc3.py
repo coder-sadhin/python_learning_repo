5dc9b42233a95c080e3b84153ad7ee8c27093fab
akkas_dev <codersadhin@gmail.com>
2024-06-15 23:31:55 +0800

    add some que

--- qu_ans/kbc3.py
+++ qu_ans/kbc3.py
@@ -95,6 +95,22 @@ questions = [
     "Which language was used to create fb?", "Python", "French", "JavaScript",
     "Php", "None", 4
   ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
 ]
 
 levels = [1000, 2000, 3000, 5000, 10000, 20000, 40000, 80000, 160000, 320000]
