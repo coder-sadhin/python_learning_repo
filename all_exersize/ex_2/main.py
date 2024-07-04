3bb4e8e5a815511db407f4675075c599a3127e71
akkas_dev <codersadhin@gmail.com>
2024-07-04 23:40:34 +0800

    change some code 2

--- all_exersize/ex_2/main.py
+++ all_exersize/ex_2/main.py
@@ -10,3 +10,9 @@ elif(hour>=12 and hour<17):
   print("Good Afternoon Sir!")
 elif(hour>=17 and hour<0):
   print("Good Night Sir!")
+# if(hour>=0 and hour<12):
+#   print("Good Morning Sir!")
+# elif(hour>=12 and hour<17):
+#   print("Good Afternoon Sir!")
+# elif(hour>=17 and hour<0):
+#   print("Good Night Sir!")
